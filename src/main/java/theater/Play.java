937d4b005704026d20afdef054709e7b25cbb95c
Anna <annamaria.castro@mail.utoronto.ca>
2025-11-13 04:26:27 -0500

    Completed Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -2,11 +2,24 @@ package theater;
 
 public class Play {
 
-    public String name;
-    public String type;
+    /**
+     * This class represents a play.
+     * @param name The name of the play.
+     * @param type The genre of the play.
+     */
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
